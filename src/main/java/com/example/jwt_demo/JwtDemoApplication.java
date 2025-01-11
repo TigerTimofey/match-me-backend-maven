5f5c9330ffc279e17b009c8da9b2def6f4de5cf7
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-01-11 19:02:56 +0200

    changes

--- src/main/java/com/example/jwt_demo/JwtDemoApplication.java
+++ src/main/java/com/example/jwt_demo/JwtDemoApplication.java
@@ -2,20 +2,12 @@ package com.example.jwt_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JwtDemoApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(JwtDemoApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		SpringApplication.run(JwtDemoApplication.class, args);
 	}
 
 }
-
